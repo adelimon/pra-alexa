b92f064e297143aea5c6d5377de905dc921cae65
Alan Delimon <alan.delimon@ellucian.com>
2020-01-21 20:42:47 -0500

    Initial commit of the skill code, hello world version.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pra-alexa
+This is a Node package that was built for asking Alexa questions about Palmyra Racing Association.
---
+++ index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const Alexa = require('ask-sdk-core');
+// use 'ask-sdk' if standard SDK module is installed
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Palmyra Racing Association', speechText)
+            .getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Palmyra Racing Association!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Palmyra Racing Association', speechText)
+            .getResponse();
+    }
+};
+
+const AboutIntentHandler ={
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AboutIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Palmyra Racing Association was founded in 1962.';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Palmyra Racing Association', speechText)
+            .getResponse();
+    }    
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Palmyra Racing Association', speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Palmyra Racing Association', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        return handlerInput.responseBuilder
+        .speak('Sorry, I can\'t understand the command. Please say again.')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+};
+
+
+exports.handler = Alexa.SkillBuilders.custom()
+     .addRequestHandlers(LaunchRequestHandler,
+                         HelloWorldIntentHandler,
+                         AboutIntentHandler,
+                         HelpIntentHandler,
+                         CancelAndStopIntentHandler,
+                         SessionEndedRequestHandler)
+     .lambda();
